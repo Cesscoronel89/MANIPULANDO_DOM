ed76cc35edcbed7fb00ac9324d1677394b03feac
Cesscoronel89 <cecycoronel89@gmail.com>
2023-03-13 23:53:21 -0300

    web completa
    lista de tareas funcional con js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-( () => { 
+import checkComplete from "./components/checkcomplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
 const btn = document.querySelector("[data-form-btn]");
-
 
 const createTask = (evento)=> {
     evento.preventDefault();
@@ -18,30 +18,14 @@ const createTask = (evento)=> {
     titleTask.classList.add("task");
     titleTask.innerText = value;
     taskContent.appendChild(titleTask);
-
-    const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`
+    //taskContent.appendChild(deleteIcon()); esto esta de mas
     //task.innerHTML = content;
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
 }
 
 //arrow functions o funciones anonimas
 btn.addEventListener("click", createTask);
-
-const checkComplete = () => {
-    const i= document.createElement("i")
-    i.classList.add("far","fa-check-square","icon");
-    i.addEventListener("click",completeTask);
-    return i
-}
-
-const completeTask = (event) =>{
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon")
-    element.classList.toggle("far");
-}
-
-}) ();
 
 
